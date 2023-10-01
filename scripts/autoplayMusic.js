1df5ded8d4f094d895139de4e326ae36f4ff61dc
Cranageist <cranageist@gmail.com>
2023-10-01 13:54:50 +0800

    Refactor

--- scripts/autoplayMusic.js
+++ scripts/autoplayMusic.js
@@ -1,27 +1,27 @@
-window.onload = pickRandomMusic;
+const musicArray = [
+  {
+    path: "./assets/audio/fly-away-thefatrat-ft-anjulie.mp3",
+    name: "Fly Away - TheFatRat ft. Anjulie",
+  },
+  {
+    path: "./assets/audio/monody-thefatrat-ft-laura-brehm.mp3",
+    name: "Monody - TheFatRat ft. Laura Brehm",
+  },
+  {
+    path: "./assets/audio/the-spectre-alan-walker.mp3",
+    name: "The Spectre - Alan Walker",
+  },
+  {
+    path: "./assets/audio/unity-thefatrat.mp3",
+    name: "Unity - TheFatRat",
+  },
+];
 
 function pickRandomMusic() {
   let musicPlayer = document.getElementById("audio-player");
 
-  const musicArray = [
-    {
-      path: "./assets/audio/fly-away-thefatrat-ft-anjulie.mp3",
-      name: "Fly Away - TheFatRat ft. Anjulie",
-    },
-    {
-      path: "./assets/audio/monody-thefatrat-ft-laura-brehm.mp3",
-      name: "Monody - TheFatRat ft. Laura Brehm",
-    },
-    {
-      path: "./assets/audio/the-spectre-alan-walker.mp3",
-      name: "The Spectre - Alan Walker",
-    },
-    {
-      path: "./assets/audio/unity-thefatrat.mp3",
-      name: "Unity - TheFatRat",
-    },
-  ];
-
   let musicIndex = Math.floor(Math.random() * musicArray.length);
   musicPlayer.setAttribute("src", musicArray[musicIndex].path);
 }
+
+window.onload = pickRandomMusic;
